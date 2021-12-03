fd50686302c42978d5fdfac3000c75bc57344f42
mackristofer <mackristofer@gmail.com>
2021-12-03 17:59:26 -0300

    Removendo imports não usado, eliminando mensagens no console

--- src/pages/MovieRoute.tsx
+++ src/pages/MovieRoute.tsx
@@ -1,10 +1,7 @@
 
-import PrivateRoute from 'components/PrivateRoute';
-import Home from 'pages/Home';
 import Movie from 'pages/Movie';
 import MovieDetails from 'pages/MovieDetails';
-import { Switch, Route, Router } from 'react-router-dom';
-import history from 'utils/history';
+import { Switch, Route } from 'react-router-dom';
 
 const MovieRoute = () => (
     <Switch>
